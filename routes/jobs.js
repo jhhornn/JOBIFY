8e6bc3e6b48d47d2dcabec5b21c3c46d0589147a
Awosise Oluwaseun <awosiseo@gmail.com>
2022-11-27 17:23:08 +0100

    new: job route setup

---
+++ routes/jobs.js
@@ -0,0 +1,18 @@
+const express = require("express")
+const JobRouter = express.Router()
+
+const { createJob, getAllJobs, getJob, updateJob, deleteJob } = require("../controllers/jobs")
+
+JobRouter
+    .route("/")
+    .post(createJob)
+    .get(getAllJobs)
+
+JobRouter
+    .route("/:id")
+    .get(getJob)
+    .patch(updateJob)
+    .delete(deleteJob)
+
+
+module.exports = JobRouter
