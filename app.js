33816e9b0d49d656d93117c26db95381388d7485
Awosise Oluwaseun <awosiseo@gmail.com>
2022-11-27 17:24:26 +0100

    update: job and auth routes added

--- app.js
+++ app.js
@@ -8,12 +8,21 @@ const {
   invalidPathHandler
 } = require("./middlewares/errHandler")
 
-
 const app = express()
+
+// routers
+const authRouter = require("./routes/auth")
+const jobRouter = require("./routes/jobs")
+
 
 app.use(express.json())
 app.use(urlencoded({ extended: false }))
 app.use(morgan("dev"))
+
+
+//routes
+app.use("/api/v1/auth", authRouter)
+app.use("/api/v1/jobs", jobRouter)
 
 app.get("/", (req, res) => {
   res.send("<h1>Job API</h1><a href='/api/v1/jobs'>Jobs route</a>")
@@ -24,6 +33,7 @@ app.all("*", (req, res, next) => {
   next()
 })
 
+// error handlers
 app.use(errorLogger)
 app.use(errorResponder)
 app.use(invalidPathHandler)
