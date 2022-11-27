9425b94084a8d63f4fd02b4ff965e50e60e2f334
Awosise Oluwaseun <awosiseo@gmail.com>
2022-11-27 17:22:13 +0100

    new: jobs controller skeleton

---
+++ controllers/jobs.js
@@ -0,0 +1,27 @@
+const createJob = async (req, res) => {
+
+}
+
+const getAllJobs = async (req, res) => {
+
+}
+
+const getJob = async (req, res) => {
+
+}
+
+const updateJob = async (req, res) => {
+
+}
+
+const deleteJob = async (req, res) => {
+
+}
+
+module.exports = {
+    createJob,
+    getAllJobs,
+    getJob,
+    updateJob,
+    deleteJob
+}
