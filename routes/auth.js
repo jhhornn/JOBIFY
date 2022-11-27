cfddda2e84d72ddda2e694e260ecf2e48060d08c
Awosise Oluwaseun <awosiseo@gmail.com>
2022-11-27 17:22:41 +0100

    new: auth route setup

---
+++ routes/auth.js
@@ -0,0 +1,14 @@
+const express = require("express")
+const AuthRouter = express.Router()
+
+const { register, login } = require("../controllers/auth")
+
+AuthRouter
+    .route("/register")
+    .post(register)
+
+AuthRouter
+    .route("/login")
+    .post(login)
+
+module.exports = AuthRouter
