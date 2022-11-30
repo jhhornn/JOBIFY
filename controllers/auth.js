ab2cde4dcf9b0176ae35ee0ee81f838f9850c66e
Awosise Oluwaseun <awosiseo@gmail.com>
2022-11-30 09:33:39 +0100

    feat: register user controller

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,12 +1,20 @@
+const User = require("../models/user")
+const { StatusCodes } = require("http-status-codes")
+const { BadRequestError } = require("../errors")
+
 const register = async (req, res) => {
-
+  //   const { name, email, password } = req.body
+  //   if (!name || !email || !password) {
+  //     throw new BadRequestError("Please provide name, email and password")
+  //   }
+  const user = await User.create({ ...req.body })
+  const token = user.createJWT()
+  res.status(StatusCodes.CREATED).json({ user: { name: user.name }, token })
 }
 
-const login = async (req, res) => {
-
-}
+const login = async (req, res) => {}
 
 module.exports = {
-    register,
-    login
+  register,
+  login
 }
