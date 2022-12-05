4be2dc9a227548714dde94390a34ffdcbd22973f
Awosise Oluwaseun <awosiseo@gmail.com>
2022-12-05 14:48:45 +0100

    feat: function to get all and a single job

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -1,27 +1,44 @@
-const createJob = async (req, res) => {
+const Job = require("../models/jobs")
+const { StatusCodes } = require("http-status-codes")
+const { BadRequestError, NotFoundError } = require("../errors")
+const { response } = require("../app")
 
+const createJob = async (req, res) => {
+  req.body.createdBy = req.user.userId
+  const job = await Job.create(req.body)
+  res.status(StatusCodes.CREATED).json({ job })
 }
 
 const getAllJobs = async (req, res) => {
-
+  const jobs = await Job.findById({ createdBy: req.user.userId }).sort(
+    "createdAt"
+  )
+  res.status(StatusCodes.OK).json({ jobs, count: jobs.length })
 }
 
 const getJob = async (req, res) => {
+  const {
+    user: { userId },
+    params: { id: jobId }
+  } = req
 
+  const job = await job.findOne({ _id: jobId, createdBy: userId })
+  if (!job) {
+    throw new NotFoundError(`No job with id ${jobId}`)
+  }
+  res.status(StatusCodes.OK).json({job})
 }
 
 const updateJob = async (req, res) => {
-
+  
 }
 
-const deleteJob = async (req, res) => {
-
-}
+const deleteJob = async (req, res) => {}
 
 module.exports = {
-    createJob,
-    getAllJobs,
-    getJob,
-    updateJob,
-    deleteJob
+  createJob,
+  getAllJobs,
+  getJob,
+  updateJob,
+  deleteJob
 }
