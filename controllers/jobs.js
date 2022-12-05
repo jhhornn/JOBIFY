e7ea5801f7c2ff26cb4d0ce8bee5b3cf6bb148f4
Awosise Oluwaseun <awosiseo@gmail.com>
2022-12-05 15:23:54 +0100

    feat: function to update job

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -30,7 +30,19 @@ const getJob = async (req, res) => {
 }
 
 const updateJob = async (req, res) => {
-  
+  const {
+    user: { userId },
+    params: { id: jobId }
+  } = req
+
+  if(company === '' || position === ''){
+    throw new BadRequestError("Company or Position field cannot be empty")
+  }
+  const job = await Job.findByIdAndUpdate({ _id: jobId, createdBy: userId}, req.body, { new: true, runValidators: true })
+  if (!job) {
+    throw new NotFoundError(`No job with id ${jobId}`)
+  }
+  res.status(StatusCodes.OK).json({job})
 }
 
 const deleteJob = async (req, res) => {}
