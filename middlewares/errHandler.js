d24d85050664d4eba19c2e02d1bf1e0cf09ec058
Awosise Oluwaseun <awosiseo@gmail.com>
2022-12-09 10:13:52 +0100

    refactor: rewrote the codes to cover more general errors

--- middlewares/errHandler.js
+++ middlewares/errHandler.js
@@ -1,4 +1,4 @@
-const { CustomAPIError } = require("../errors")
+// const { CustomAPIError } = require("../errors")
 const { StatusCodes } = require("http-status-codes")
 
 const errorLogger = (err, req, res, next) => {
@@ -7,10 +7,33 @@ const errorLogger = (err, req, res, next) => {
 }
 
 const errorResponder = (err, req, res, next) => {
-  res.header("Content-Type", "application/json")
-  if (err instanceof CustomAPIError) {
-    return res.status(err.statusCode).json({ msg: err.message })
+  let customError = {
+    statusCode: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
+    msg: err.message || "Something went wrong try again later"
   }
+  // if (err instanceof CustomAPIError) {
+  //   return res.status(err.statusCode).json({ message: err.message })
+  // }
+  if (err.name == "ValidationError") {
+    customError.msg = Object.values(err.errors)
+      .map((item) => item.message)
+      .join(",")
+    customError.statusCode = 400
+  }
+  if (err.code && err.code === 11000) {
+    customError.msg = `Duplicate value for ${Object.keys(
+      err.keyValue
+    )} field, please choose another value`
+    customError.statusCode = 400
+  }
+  if (errorResponder.name === "CastError") {
+    customError.msg = `No item found with id : ${err.value}`
+    customError.statusCode = 404
+  }
+  return res.status(customError.statusCode).json({ msg: customError.msg })
+  // else {
+  //   next(err)
+  // }
 }
 
 const invalidPathHandler = (err, req, res, next) => {
